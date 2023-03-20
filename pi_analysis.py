33b77bd363400fbc6210336112752b1b671b07a5
miles <miles4599@gmail.com>
2023-03-20 01:28:54 -0700

    added some .txt files with pi, wrote function to calculate longest consecutive string of a digit

---
+++ pi_analysis.py
@@ -0,0 +1,38 @@
+import string
+
+class Consecutive:
+    length = 0
+    location = 0
+    num = '0'
+
+pi_str = ""
+with open("pi_million.txt") as f:
+    pi_str = f.read()
+
+def longest_repetition(pi:string) -> Consecutive:
+    result = Consecutive()
+    max = 0
+    for i in range(len(pi) - 1):
+        start = i
+        end = i + 1
+        if pi[start] == pi[end]:
+            count = 1
+            while pi[start] == pi[end]:
+                count += 1
+                end += 1
+            if count > max:
+                result.num = pi[start]
+                result.location = start
+                max = count
+
+    result.length = max
+
+    return result
+
+results = longest_repetition(pi_str);
+
+print(f"Length:   {results.length}")
+print(f"Location: {results.location}")
+print(f"Number:    {results.num}")
+print(len(pi_str))
+print(longest_repetition(pi_str))
