4ab59096973a6e3609e99332d7880a51ddfecf9b
miles <miles4599@gmail.com>
2023-03-20 14:07:05 -0700

    added pi_10_million.txt, finished working code to find longest consecutive string

--- pi_analysis.py
+++ pi_analysis.py
@@ -6,17 +6,33 @@ class Consecutive:
     num = '0'
 
 pi_str = ""
-with open("pi_100_million.txt") as f:
+with open("pi_10_million.txt") as f:
     pi_str = f.read()
+
+
+def count_each_digit(pi: string) -> list['int']:
+    counts = [0] * 10
+    for i in range(len(pi) - 1):
+        count_digit(int(pi[i]), counts)
+    return counts
+    
+
+def count_digit(digit: int, counts: list['int']): 
+    counts[digit] += 1
+
 
 def longest_repetition(pi:string) -> Consecutive:
     result = Consecutive()
     max = 0
+    ## iterate through each digit
     for i in range(len(pi) - 1):
         start = i
         end = i + 1
+        # compare the digit with the next and see if they're the same
         if pi[start] == pi[end]:
             count = 1
+            # as long as they're the same, compare the digit after that against the original digit
+            # keeping a count as you go 
             while pi[start] == pi[end]:
                 count += 1
                 end += 1
@@ -24,15 +40,29 @@ def longest_repetition(pi:string) -> Consecutive:
                 result.num = pi[start]
                 result.location = start
                 max = count
-
+        # skip the string we just counted
+        i += max
     result.length = max
-
     return result
 
-results = longest_repetition(pi_str);
 
+counts = count_each_digit(pi_str)
+results = longest_repetition(pi_str)
+
+
+
+print("\nDigits of pi calculated: ")
+print("---------------------------------------")
+print(f"{len(pi_str) - 2}")
+
+print("\nCount of each digit: ")
+print("---------------------------------------")
+for i in range(10):
+    print(f"{i}:        {counts[i]}")
+print("\nLongest consecutive repeated digit: ")
+print("---------------------------------------")
 print(f"Length:   {results.length}")
+print(f"Number:   {results.num}")
 print(f"Location: {results.location}")
-print(f"Number:    {results.num}")
-print(len(pi_str))
-print(longest_repetition(pi_str))
+print()
+
